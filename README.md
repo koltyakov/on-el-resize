fd5804d4288e482136deef1b6d18de357aa898b5
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2018-08-24 13:58:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# on-el-resize - on HTML element resize event helper
+
+`on-el-resize` is a simple helper which provides an API for attaching resize event firing to HTML elements.
+
+This can simplify creation of some dynamic and responsive components which styles and structure depends on absolute width and height values.
+
+## Install
+
+```bash
+npm install on-el-resize # or yarn add on-el-resize
+```
+
+## Usage
+
+### TypeScript / JavaScript
+
+```typescript
+import { Resize } from 'on-el-resize';
+
+const resize = new Resize();
+const element = document.querySelector('.selector');
+
+const resizeHandler = () => {
+  console.log({
+    width: element.offsetWidth,
+    heigth: element.offsetHeight
+  });
+};
+
+// Attach event
+resize.addResizeListener(element, resizeHandler);
+
+// Detach event
+resize.removeResizeListener(element, resizeHandler);
+```
+
+### React component
+
+```typescript
+import * as React from 'react';
+import { Resize } from 'on-el-resize';
+
+export interface IProps {}
+
+export interface IState {
+  componentWidth?: number;
+  componentHeight?: number;
+}
+
+export class Example extends React.Component<IProps, IState> {
+
+  private parentRef: HTMLElement;
+  private resize: Resize;
+
+  constructor(props: IProps) {
+    super(props);
+    this.resize = new Resize();
+    this.state = {};
+    this.onComponentResize = this.onComponentResize.bind(this);
+  }
+
+  public render() {
+    return (
+      <div ref={el => this.parentRef = el}>
+        <div>Width: {this.state.componentWidth}</div>
+        <div>Height: {this.state.componentHeight}</div>
+      </div>
+    );
+  }
+
+  public componentDidMount() {
+    this.resize.addResizeListener(this.parentRef, this.onComponentResize);
+    this.onComponentResize();
+  }
+
+  public componentWillUnmount() {
+    this.resize.removeResizeListener(this.parentRef, this.onComponentResize);
+  }
+
+  private onComponentResize() {
+    this.setState({
+      componentWidth: this.parentRef.offsetWidth,
+      componentHeight: this.parentRef.offsetHeight
+    });
+  }
+
+}
+```
---
+++ src/Resize.ts
@@ -0,0 +1,73 @@
+export class Resize {
+
+  private attachEvent = document['attachEvent'];
+  private isIE = navigator.userAgent.match(/Trident/);
+
+  public addResizeListener = (element: HTMLElement, fn: Function): void => {
+    if (!element['__resizeListeners__']) {
+      element['__resizeListeners__'] = [];
+      if (this.attachEvent) {
+        element['__resizeTrigger__'] = element;
+        element['attachEvent']('onresize', this.resizeListener);
+      } else {
+        if (getComputedStyle(element).position === 'static') element.style.position = 'relative';
+        const obj = element['__resizeTrigger__'] = document.createElement('object');
+        obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
+        obj['__resizeElement__'] = element;
+        obj.onload = this.objectLoad.bind(this);
+        obj.type = 'text/html';
+        if (this.isIE) {
+          element.appendChild(obj);
+        }
+        obj.data = 'about:blank';
+        if (!this.isIE) {
+          element.appendChild(obj);
+        }
+      }
+    }
+    element['__resizeListeners__'].push(fn);
+  }
+
+  public removeResizeListener = (element: HTMLElement, fn: Function): void => {
+    element['__resizeListeners__'].splice(element['__resizeListeners__'].indexOf(fn), 1);
+    if (!element['__resizeListeners__'].length) {
+      if (this.attachEvent) {
+        element['detachEvent']('onresize', this.resizeListener);
+      } else {
+        element['__resizeTrigger__'].contentDocument.defaultView.removeEventListener('resize', this.resizeListener);
+        element['__resizeTrigger__'] = !element.removeChild(element['__resizeTrigger__']);
+      }
+    }
+  }
+
+  private requestFrame = (() => {
+    const rafFallback = (fn: Function) => window.setTimeout(fn, 20);
+    const raf = window.requestAnimationFrame || window['mozRequestAnimationFrame'] || window.webkitRequestAnimationFrame || rafFallback;
+    return (fn: Function) => raf(fn);
+  })();
+
+  private cancelFrame = (() => {
+    const cancel = window.cancelAnimationFrame || window['mozCancelAnimationFrame'] || window.webkitCancelAnimationFrame || window.clearTimeout;
+    return id => cancel(id);
+  })();
+
+  private resizeListener = (e): void => {
+    const win = e.target || e.srcElement;
+    if (win.__resizeRAF__) {
+      this.cancelFrame(win.__resizeRAF__);
+    }
+    win.__resizeRAF__ = this.requestFrame(() => {
+      const trigger = win['__resizeTrigger__'];
+      trigger['__resizeListeners__'].forEach(fn => {
+        fn.call(trigger, e);
+      });
+    });
+  }
+
+  private objectLoad = (ev: Event): void => {
+    const el = ev.target;
+    el['contentDocument'].defaultView['__resizeTrigger__'] = el['__resizeElement__'];
+    el['contentDocument'].defaultView.addEventListener('resize', this.resizeListener);
+  }
+
+}
